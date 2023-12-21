91d84f8a065889f7f2ee8f1426e1710759b0081f
Yoav Farhi <yoav@farhi.org>
2023-12-21 12:13:45 +0200

    fix linter issue regarding useEffect deps

--- app/components/ExampleMessages.tsx
+++ app/components/ExampleMessages.tsx
@@ -6,21 +6,21 @@ interface Props {
 }
 
 export default function ExampleMessages({ onClick, count }: Props) {
-  const exampleMessages = [
-    "Explain quantum computing in simple terms",
-    "Got any creative ideas for a 10 year old's birthday?",
-    "How do I make an HTTP request in Javascript?",
-    "What is the Fermi Paradox and what are its implications?",
-    "How does an airplane fly? Explain the aerodynamics in simple terms.",
-    "What's an example of a paradox that's hard to understand but true?",
-    "Explain the theory behind time travel. Is it scientifically plausible?",
-    "What are some effective strategies for conflict resolution?",
-    "Compose a haiku poem about artificial intelligence.",
-  ];
-
   const [randomExamples, setRandomExamples] = useState<string[]>([]);
 
   useEffect(() => {
+    const exampleMessages = [
+      "Explain quantum computing in simple terms",
+      "Got any creative ideas for a 10 year old's birthday?",
+      "How do I make an HTTP request in Javascript?",
+      "What is the Fermi Paradox and what are its implications?",
+      "How does an airplane fly? Explain the aerodynamics in simple terms.",
+      "What's an example of a paradox that's hard to understand but true?",
+      "Explain the theory behind time travel. Is it scientifically plausible?",
+      "What are some effective strategies for conflict resolution?",
+      "Compose a haiku poem about artificial intelligence.",
+    ];
+
     const shuffled = [...exampleMessages].sort(() => 0.5 - Math.random());
     setRandomExamples(shuffled.slice(0, count));
   }, [count]);
